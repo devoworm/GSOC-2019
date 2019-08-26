4463a0bbb3a2b6277d0f514c9b13ec843cbd4dec
Vinay Varma <vinay.n.varma189@gmail.com>
2019-08-26 22:45:20 +0530

    add documentation for code

--- OpenDevoCell/app.py
+++ OpenDevoCell/app.py
@@ -16,8 +16,8 @@ def home():
         if 'file' not in request.files:
             return "No file found"
         final_list = {}
-        c = 0
-        row = 0
+        c = 0 #counter variable
+        row = 0 #counter for appending rows into dataframe
         print('11')
         global df
         df = df[0:0]
@@ -25,46 +25,45 @@ def home():
             print(i, end='\n')
             file_name_str = str(i.filename)
             file_name_str = file_name_str.split('.')[0]
-            img = i.read()
+            img = i.read() #Read image from FileStorage Object
 
-            img = cv2.imdecode(np.frombuffer(img, np.uint8), -1)
-            img = cv2.resize(img, (390, 620))
+            img = cv2.imdecode(np.frombuffer(img, np.uint8), -1) #convert to openCV image
+            img = cv2.resize(img, (390, 620)) # resize all images to (390, 620)
+
             # blur = cv2.medianBlur(img, 11) # median blurring
             blur = cv2.bilateralFilter(img,9,75,75) # bilateralFilter blurring (seems to have less noise some times)
 
             # Gradient Morphology
             kernel = np.ones((3,3),np.uint8)
-            gradient = cv2.morphologyEx(blur, cv2.MORPH_GRADIENT, kernel)
-            op = cv2.morphologyEx(gradient, cv2.MORPH_OPEN, kernel)
-            gray = cv2.cvtColor(op, cv2.COLOR_BGR2GRAY)
+            gradient = cv2.morphologyEx(blur, cv2.MORPH_GRADIENT, kernel) # apply gradient morphology
+            op = cv2.morphologyEx(gradient, cv2.MORPH_OPEN, kernel) # apply opening morphology
+            gray = cv2.cvtColor(op, cv2.COLOR_BGR2GRAY) # convert to grayscale
 
-            ret,thresh1 = cv2.threshold(gray,1,255,cv2.THRESH_BINARY)
+            ret,thresh1 = cv2.threshold(gray,1,255,cv2.THRESH_BINARY) # thresholding (convert to binary)
 
-            conts, hierarchy = cv2.findContours(thresh1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            conts, hierarchy = cv2.findContours(thresh1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) # extract contours
             filter_c = []
             for i in range(len(conts)):
-                area = cv2.contourArea(conts[i])
+                area = cv2.contourArea(conts[i]) #filter out small noises
                 if area>500:
-                    if (hierarchy[0][i][3] != -1):
+                    if (hierarchy[0][i][3] != -1): # ignore the contour of the whole cell
                         filter_c.append(conts[i])
-                        # filter_h.append(hierarchy[0][i])
             for j in range(len(filter_c)):
-                # compute the center of the contour
-                M = cv2.moments(filter_c[j])
+                M = cv2.moments(filter_c[j]) # compute the center of the contour
                 area = cv2.contourArea(filter_c[j])
-                cX = int(M["m10"] / M["m00"])
-                cY = int(M["m01"] / M["m00"])
+                cX = int(M["m10"] / M["m00"]) # X coordinate
+                cY = int(M["m01"] / M["m00"]) # Y coordinate
 
-                df.loc[row] = [file_name_str] + ['cell'+str(j+1)] + [cX] + [cY] + [area]
+                df.loc[row] = [file_name_str] + ['cell'+str(j+1)] + [cX] + [cY] + [area] # add them to df dataframe
                 row+=1
-                cv2.drawContours(img, [filter_c[j]], -1, (0, 255, 0), 4)
-                cv2.circle(img, (cX, cY), 4, (255, 255, 255), -1)
+                cv2.drawContours(img, [filter_c[j]], -1, (0, 255, 0), 4) # draw extracted contours to display result images
+                cv2.circle(img, (cX, cY), 4, (255, 255, 255), -1) #draw centroids
             print("111")
             print(df)
-            ret, buf = cv2.imencode( '.jpg', img )
+            ret, buf = cv2.imencode( '.jpg', img ) # convert openCV image to byte array
 
             img_string = base64.b64encode(buf)
-            img_string = img_string.decode("utf-8")
+            img_string = img_string.decode("utf-8") # convet byte array to base64 string
 
             final_list[str(c)] = img_string
             c+=1
--- Unsupervised_approach/example-legacy-plugin/src/main/java/com/mycompany/imagej/Process_Pixels.java
+++ Unsupervised_approach/example-legacy-plugin/src/main/java/com/mycompany/imagej/Process_Pixels.java
@@ -51,7 +51,7 @@ public class Process_Pixels implements PlugInFilter {
 				e.printStackTrace();
 			}
 	}
-	
+
 
 	// load the tensorflow model.
 	public static void loadModel(String filepath) throws Exception{
@@ -60,13 +60,13 @@ public class Process_Pixels implements PlugInFilter {
             file = new ClassPathResource(filepath).getFile();
         }
 
-        sd = TFGraphMapper.getInstance().importGraph(file);
+        sd = TFGraphMapper.getInstance().importGraph(file); // load the model as a same diff graph
 
         if (sd == null) {
             throw new Exception("Error loading model : " + file);
         }
     }
-	
+
 	// resize a buffered image
 	public static BufferedImage resize(BufferedImage img, int height, int width) {
         Image tmp = img.getScaledInstance(width, height, Image.SCALE_SMOOTH);
@@ -76,76 +76,76 @@ public class Process_Pixels implements PlugInFilter {
         g2d.dispose();
         return resized;
     }
-	
+
 	// pass the input image to the model and get the output
 	public static INDArray predict (BufferedImage input_b) throws IOException{
 
 
         BufferedImage bimage = input_b;
 
-        BufferedImage img = resize(bimage, 224, 224);
+        BufferedImage img = resize(bimage, 224, 224); //resize image to size of training images
 
-        
+
         float data[] = new float[img.getWidth() * img.getHeight()];
-        
+
         for(int i = 0; i < img.getWidth(); i++){
             for(int j = 0; j < img.getHeight(); j++){
             	int p = (img.getRGB(i, j));
 
-                int r = (p>>16)&0xff; 
-                int g = (p>>8)&0xff; 
+                int r = (p>>16)&0xff;
+                int g = (p>>8)&0xff;
                 int b = p&0xff;
 
-                float greyScale = (r + g + b) / 3;
-                greyScale /= 255.;
+                float greyScale = (r + g + b) / 3; //convert to greyscale
+                greyScale /= 255.; // normalize each of the image
                 data[i * img.getWidth() + j] = greyScale;
             }
         }
-        
-        
-        
-        INDArray arr = Nd4j.create(data).reshape(1, 224, 224, 1);
 
-        sd.associateArrayWithVariable(arr, sd.variables().get(0));
-        
-        INDArray output = sd.execAndEndResult();
-        
+
+
+        INDArray arr = Nd4j.create(data).reshape(1, 224, 224, 1); //create INDarray with input image as data
+
+        sd.associateArrayWithVariable(arr, sd.variables().get(0)); //attach arr to the first layer of the graph
+
+        INDArray output = sd.execAndEndResult(); //make the prediction
+
         return output;
     }
-	
+
 	// driver method
 	public void process(ImageProcessor ip) throws Exception{
 		BufferedImage output_b = null;
-		BufferedImage input_b = ip.getBufferedImage();
-		
-		
-		int original_x = input_b.getHeight();
-		int original_y = input_b.getWidth();
-		
-    	
-        loadModel("wnetmodel_1.13.1.pb");
-        
-        
-        INDArray prediction = predict(input_b);
-        
-        
-        prediction = prediction.reshape('c', 224, 224, 3);
-        
+		BufferedImage input_b = ip.getBufferedImage(); //convert input image to buffered image
 
-        
-        float[][][] out = new float[(int) prediction.size(0)][0][0];
+
+		int original_x = input_b.getHeight(); // store height of the input image
+		int original_y = input_b.getWidth(); // store width of the input image
+
+
+        loadModel("wnetmodel_1.13.1.pb");
+
+
+        INDArray prediction = predict(input_b); // pass the input image into the model
+
+
+        prediction = prediction.reshape('c', 224, 224, 3); // reshape prediction
+
+
+
+        float[][][] out = new float[(int) prediction.size(0)][0][0]; //convert INDArray to float matrix
         for(int i=0; i<(int) prediction.size(0); i++ ){
             out[i] = prediction.get(NDArrayIndex.point(i), NDArrayIndex.all(), NDArrayIndex.all()).toFloatMatrix();
         }
-        
-       
+
+
         int xLength = out.length;
         int yLength = out[0].length;
         int zLength = out[0][0].length;
-        
+
         output_b = new BufferedImage(xLength, yLength, 4);
-        
-        for(int x = 0; x < xLength; x++) {
+
+        for(int x = 0; x < xLength; x++) { //post process which balances normalization
             for(int y = 0; y < yLength; y++) {
                 for(int z = 0; z < zLength; z++)
                 {
@@ -153,15 +153,15 @@ public class Process_Pixels implements PlugInFilter {
                 }
             }
         }
-        
-       
-        
-        for(int x = 0; x < xLength; x++) {
+
+
+
+        for(int x = 0; x < xLength; x++) { //store buffered image with RGB values obtained from model prediction.
             for(int y = 0; y < yLength; y++) {
                 	int red = (int) out[x][y][0];
                 	int green = (int) out[x][y][1];
                 	int blue = (int) out[x][y][2];
-                	
+
                 	int rgb = red;
                 	rgb = (rgb << 8) + green;
                 	rgb = (rgb << 8) + blue;
@@ -169,17 +169,17 @@ public class Process_Pixels implements PlugInFilter {
             }
         }
         File outputfile = new File("output.png");
-        
-        
-        output_b = resize(output_b, original_x, original_y);
-        
-                
+
+
+        output_b = resize(output_b, original_x, original_y); //resize the output image to original input image size
+
+
         ImagePlus output_imageplus = new ImagePlus("Segmented Output", output_b);
-        
-        output_imageplus.show();
-        
+
+        output_imageplus.show(); //show the output image when user runs plugin
+
         ImageIO.write(output_b, "png", outputfile);
-        
+
 	}
 
 
@@ -190,7 +190,7 @@ public class Process_Pixels implements PlugInFilter {
 	}
 
 
-	
+
 	public static void main(String[] args) throws Exception {
 
 		Class<?> clazz = Process_Pixels.class;
